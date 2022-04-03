5bfe62c73a7cb64381d54c38ef755a0255d477fd
mostofa kamal <mostofakamal1990@gmail.com>
2022-04-04 02:51:22 +0600

    custom hook created

--- src/components/LineCharts/LineCharts.js
+++ src/components/LineCharts/LineCharts.js
@@ -1,22 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+import useChartData from '../../hooks/useChartData';
+
 
 const LineCharts = () => {
-    const [data, setData] = useState([]);
+    const [data, setData] = useChartData([]);
 
-    useEffect(()=>{
-        fetch('data.json').then(data => data.json()).then(data => setData(data));
-
-    },[])
     
     return (
+        <>
+            <h2 className='text-primary text-center'>MONTHLY WISE SELL</h2>
         <LineChart width={600} height={300} data={data}>
     <Line type="monotone" dataKey="sell" stroke="#8884d8" />
     <CartesianGrid stroke="#ccc" />
     <XAxis dataKey="month" />
     <YAxis/>
   </LineChart>
+  </>
     );
+
 };
 
 export default LineCharts;
---
+++ src/hooks/useChartData.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+
+const useChartData = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(()=>{
+        fetch('data.json').then(data => data.json()).then(data => setData(data));
+
+    },[]);
+
+    return [data, setData] ;
+
+};
+
+export default useChartData;
