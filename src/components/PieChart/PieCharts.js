7747730ab8297cfb0823f2741db5dbbb717e4587
mostofa kamal <mostofakamal1990@gmail.com>
2022-04-04 03:25:58 +0600

    pi chart add complete

--- src/components/PieChart/PieCharts.js
+++ src/components/PieChart/PieCharts.js
@@ -3,18 +3,38 @@ import { Pie, PieChart, ResponsiveContainer } from 'recharts';
 import useChartData from '../../hooks/useChartData';
 
 const PieCharts = () => {
+;
     const [data, setData] = useChartData();
+    const [data1, setData2] = useChartData();
     return (
-        <>
+        <div>
     <h2 className='text-primary'>Investment VS Revenue</h2>
-<ResponsiveContainer width="100%" height="100%">
-        <PieChart width={400} height={400}>
-          <Pie data={data} dataKey="investment" cx="50%" cy="50%" outerRadius={60} fill="#8884d8" />
-          <Pie data={data} dataKey="revenue" cx="50%" cy="50%" innerRadius={70} outerRadius={90} fill="#82ca9d" label />
-        </PieChart>
-      </ResponsiveContainer>
+{/* <ResponsiveContainer width="100%" height="100%"> */}
+<PieChart width={400} height={400}>
+      <Pie
+        data={data}
+        dataKey="revenue"
+        cx={200}
+        cy={200}
+        outerRadius={60}
+        fill="#8884d8"
+        
+      />
+      <Pie
+        data={data1}
+        dataKey="revenue"
+        cx={200}
+        cy={200}
+        innerRadius={70}
+        outerRadius={90}
+        fill="#82ca9d"
+        label
+      />
+    </PieChart>
 
-        </>
+      {/* </ResponsiveContainer> */}
+
+        </div>
     );
 };
 
