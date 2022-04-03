b73a8bd730049d0e9cc57d742abb09414880fb0f
mostofa kamal <mostofakamal1990@gmail.com>
2022-04-04 00:23:02 +0600

    route part add in header component and app component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Header from './components/Header/Header';
 import Products from './components/Products/Products';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <>
       <Header></Header>
+      <Routes>
+         <Route path='/' element={<Home></Home>}></Route>
+         <Route path='/blogs' element={<Home></Home>}></Route>
+         <Route path='/Home' element={<Home></Home>}></Route>
+         <Route path='/reviews' element={<Home></Home>}></Route>
+         <Route path='/about' element={<Home></Home>}></Route>
+         <Route path='/dashboard' element={<Home></Home>}></Route>
+      </Routes>
 
       <Products></Products>
     
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -5,7 +5,7 @@ import { Col, Container, Row ,Button} from 'react-bootstrap';
 const Products = () => {
     return (
         <div>
-            <Container  className='px-auto mx-auto'>
+            <Container  className='px-auto mx-auto my-5'>
                 <Row>
                     <Col className='col-sm-6'>
                        <img src={'./images/drone-1.webp'} alt="" />
@@ -16,7 +16,7 @@ const Products = () => {
                             </span>  <br></br> Next Generation Military Drone Systems</h1>
 
                             <p>
-                            The field of unmanned combat aerial vehicles (UCAVs) is rapidly diversifying as an array of light, mid-sized, and heavy UCAVs and munitions enter the global marketplace.
+                            There is an expansion of munitions being equipped on military drones and different munitions are being tested on drones,” Gettinger says. “For a long time, we had the archetypal Predator drone, but the field of armed drones has diversified to include smaller and larger aircraft, so the family of munitions has also diversified. We see more variety in the types of munitions on them; we’ll see further automation demonstrated in the loitering munitions
                             </p>
 
                             <Button variant="secondary">Secondary</Button>
