4e84e006eb64a447129eab7aba5e4deea5e97e39
mostofa kamal <mostofakamal1990@gmail.com>
2022-04-05 01:00:38 +0600

    review component show product

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { Container, Row, Col, Card } from "react-bootstrap";
+import './Reviews.css';
 
 const Reviews = () => {
-    return (
-        <div>
-            <h2>ReViews</h2>
-        </div>
-    );
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("reviews.json")
+      .then((data) => data.json())
+      .then((data) => setData(data));
+  }, []);
+
+  return (
+    <Container className="my-3 pt-5">
+         <h2 className="text-center">What our Customers say!</h2>
+      <Row xs={1} md={2} className="g-4">
+       
+        {data.map((da) => (
+          <Col sm={6} className='my-3 mt-5' key={da.Id}>
+            <Card border="primary" className="h-100">
+              <Card.Header>
+                  <img src={da.image} alt="" />
+              </Card.Header>
+              <Card.Body className="mt-5">
+               
+                <Card.Text>
+                 {da.comment}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+
+      {}
+    </Container>
+  );
 };
 
 export default Reviews;
