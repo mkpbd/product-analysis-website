b70ad70442c3b87e93e0369d46ece2f2a3350e5d
mkpbd <mostofakamal1990@gmail.com>
2022-04-05 14:48:55 +0600

    Read me file edite

--- README.md
+++ README.md
@@ -29,7 +29,12 @@ Your app is ready to be deployed!
 
 See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
 
-### `npm run eject`
+### `Project Description`
+    * Drone  App
+    * User Review 
+    * Blogs
+    * Product section
+    
 
 **Note: this is a one-way operation. Once you `eject`, you can't go back!**
 
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -4,9 +4,9 @@ const NotFound = () => {
   return (
     <div className="container py-5 my-5">
       <div className="row">
-        <div className="col-sm-8 offset-2 my-4">
+        <div className="col-sm-8 offset-2 my-4 shadow-lg p-3 mb-5 bg-body rounded">
           <img
-            className="d-block text-center"
+            className="d-block text-center img-fluid"
             src="images/404.png"
             alt=""
           />
